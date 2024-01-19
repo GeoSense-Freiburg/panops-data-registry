4dd3040786054c4596e65897e8bfd2e5e66c3ac6
Daniel Lusk <daniel.lusk@posteonet>
2024-01-19 11:47:58 +0000

    add GBIF download functionality

---
+++ src/data/download_gbif.py
@@ -0,0 +1,153 @@
+"""Initiate a GBIF download and save it to disk once it is ready."""
+import json
+import logging
+import time
+from pathlib import Path
+from typing import Optional
+
+import click
+from dotenv import find_dotenv, load_dotenv
+from pygbif import occurrences as occ
+
+log = logging.getLogger(__name__)
+
+
+class GbifDownloadFailure(Exception):
+    """Exception raised when a GBIF download job fails."""
+
+    def __init__(self, message="GBIF download job failed."):
+        self.message = message
+        super().__init__(self.message)
+
+
+def init_gbif_download(query: dict) -> str:
+    """
+    Initiate a GBIF download job and return the job key.
+    """
+    download_key = occ.download(query)  # type: ignore
+    return download_key[0]
+
+
+def check_download_status(key: str) -> str:
+    """Check the status of a GBIF download job."""
+    status = occ.download_meta(key)
+    return status["status"]
+
+
+def download_request_to_disk(
+    key: str, output_path: Path, name: Optional[str] = None
+) -> None:
+    """Download a completed GBIF download job's zipfile and metadata."""
+
+    occ.download_get(key, str(output_path))
+
+    # This is how it is downloaded by default
+    output_full_path = output_path / f"{key}.zip"
+
+    if name is not None:
+        # Rename output file with name
+        named_file = output_path / f"{name}.zip"
+        output_full_path = output_full_path.rename(named_file)
+
+    # Also save the metadata alongside the data
+    with open(
+        output_path / f"{key if name is None else name}.json",
+        "w",
+        encoding="utf-8",
+    ) as f:
+        json.dump(occ.download_meta(key), f)
+
+
+def check_download_job_and_download_file(
+    key: str, output_path: Path, name: Optional[str] = None, max_hours: int | float = 6
+) -> None:
+    """
+    Checks a pending GBIF download for a given amount of time, downloads the file once
+    it is ready.
+
+    Args:
+        output_path (Path): The path where the downloaded file will be saved.
+        key (str): The key of the GBIF download job to check.
+        max_hours (int | float, optional): The maximum number of hours to wait for the download
+            to complete. Defaults to 6.
+
+    Raises:
+        GbifDownloadFailure: If the download job fails or exceeds the maximum waiting time.
+
+    """
+    start_time = time.time()
+    while True:
+        status = check_download_status(key)
+        if status == "SUCCEEDED":
+            download_request_to_disk(key=key, output_path=output_path, name=name)
+            break
+
+        if status == "FAILED":
+            raise GbifDownloadFailure(f"Download job {key} failed.")
+
+        if time.time() - start_time > max_hours * 60 * 60:
+            output_path.unlink()
+            raise GbifDownloadFailure(
+                f"Download job {key} did not complete within" f"{max_hours}. Aborting"
+            )
+        time.sleep(60)
+
+
+@click.command()
+@click.option(
+    "-q",
+    "--query",
+    "query_file",
+    type=click.Path(path_type=Path),
+    default=Path("references/gbif/all_tracheophyta.json"),
+    show_default=True,
+)
+@click.option(
+    "-n",
+    "--name",
+    "name",
+    type=click.STRING,
+    help="Human-readable name of the query. Will be used as the download name.",
+)
+@click.option(
+    "-k",
+    "--key",
+    "key",
+    type=click.STRING,
+    help="Key of download already created. Will simply re-download.",
+)
+@click.option(
+    "-o",
+    "--output",
+    "output_path",
+    type=click.Path(exists=True, file_okay=False, path_type=Path),
+    default=Path().cwd(),
+    show_default=True,
+)
+def main(query_file: Path, name: str, key: str, output_path: Path):
+    """Check the status of a GBIF download job and download the CSV file once it's ready."""
+    load_dotenv(find_dotenv())  # Find local .env to expose GBIF credentials
+
+    if query_file.suffix != ".json":
+        raise ValueError("Query file must be a .json file.")
+
+    if not output_path.is_dir():
+        raise ValueError("Output path must be a directory.")
+
+    with open(query_file, "r", encoding="utf-8") as f:
+        gbif_query = json.load(f)
+
+    if key is None:
+        log.info("No key provided. Initializing new query.")
+        download_key = init_gbif_download(gbif_query)
+    else:
+        log.info("Re-checking download using key: %s", key)
+        download_key = key
+    log.info("Checking if download job is ready...")
+    check_download_job_and_download_file(
+        key=download_key, output_path=output_path, name=name
+    )
+
+
+if __name__ == "__main__":
+    main()  # pylint: disable=no-value-for-parameter
---
+++ tests/data/test_download_gbif.py
@@ -0,0 +1,226 @@
+"""Test src.data.download_gbif"""
+import time
+from pathlib import Path
+
+import pytest
+from pygbif import occurrences as occ
+
+from src.data.download_gbif import (
+    GbifDownloadFailure,
+    check_download_job_and_download_file,
+    check_download_status,
+    download_request_to_disk,
+    init_gbif_download,
+)
+
+# Define the module name to be used for mocking
+TESTED_MODULE = "src.data.download_gbif"
+
+
+@pytest.fixture(name="gbif_query")
+def fixture_gbif_query() -> dict:
+    """Sample GBIF query"""
+    return {
+        "type": "and",
+        "predicates": [
+            {
+                "type": "equals",
+                "key": "TAXON_KEY",
+                "value": "2653316",  # Ephedra equisetina, only one record
+            },
+            {"type": "not", "key": "DEGREE_OF_ESTABLISHMENT", "value": "cultivated"},
+        ],
+    }
+
+
+@pytest.fixture(name="gbif_download_info")
+def fixture_gbif_download_info() -> tuple:
+    """Sample GBIF download info"""
+    return (
+        "0000000-000000000000000",
+        {
+            "creator": "lusk",
+            "notification_address": ["daniel.lusk@geosense.uni-freiburg.de"],
+            "sendNotification": True,
+            "predicate": {
+                "type": "and",
+                "predicates": [
+                    {"type": "equals", "key": "TAXON_KEY", "value": "2653316"},
+                    {
+                        "type": "not",
+                        "predicate": {
+                            "type": "in",
+                            "key": "DEGREE_OF_ESTABLISHMENT",
+                            "values": ["cultivated"],
+                        },
+                    },
+                ],
+            },
+            "format": "SIMPLE_CSV",
+        },
+    )
+
+
+@pytest.fixture(name="gbif_download_meta_running")
+def fixture_gbif_download_meta_running() -> dict:
+    """Sample GBIF download meta in a RUNNING state."""
+    return {
+        "key": "0000000-000000000000000",
+        "doi": "10.15468/dl.4fm52d",
+        "license": "http://creativecommons.org/licenses/by-nc/4.0/legalcode",
+        "request": {
+            "predicate": {
+                "type": "and",
+                "predicates": [
+                    {
+                        "type": "equals",
+                        "key": "TAXON_KEY",
+                        "value": "7707728",
+                        "matchCase": False,
+                    },
+                    {
+                        "type": "not",
+                        "predicate": {
+                            "type": "in",
+                            "key": "DEGREE_OF_ESTABLISHMENT",
+                            "values": ["cultivated"],
+                            "matchCase": False,
+                        },
+                    },
+                ],
+            },
+            "sendNotification": True,
+            "format": "SIMPLE_CSV",
+            "type": "OCCURRENCE",
+            "verbatimExtensions": [],
+        },
+        "created": "2024-01-17T14:18:26.685+00:00",
+        "modified": "2024-01-17T14:31:12.445+00:00",
+        "eraseAfter": "2024-07-17T14:18:26.641+00:00",
+        "status": "RUNNING",
+        "downloadLink": "https://api.gbif.org/v1/occurrence/download/request/"
+        "0000000-000000000000000.zip",
+        "size": 50765,
+        "totalRecords": 1,
+        "numberDatasets": 1,
+    }
+
+
+@pytest.fixture(name="gbif_download_meta_success")
+def fixture_gbif_download_meta_success(gbif_download_meta_running) -> dict:
+    """Sample GBIF download meta in a SUCCEEDED state."""
+    return {**gbif_download_meta_running, "status": "SUCCEEDED"}
+
+
+@pytest.fixture(name="gbif_download_meta_failed")
+def fixture_gbif_download_meta_failed(gbif_download_meta_running) -> dict:
+    """Sample GBIF download meta in a FAILED state."""
+    return {**gbif_download_meta_running, "status": "FAILED"}
+
+
+@pytest.fixture(name="mock_download_meta")
+def fixture_mock_download_meta(monkeypatch, gbif_download_meta_success: dict):
+    """Mock pygbif.occ.download_meta to return a successful download meta."""
+
+    def _mock_download_meta(*args, **kwargs):
+        return gbif_download_meta_success
+
+    monkeypatch.setattr(occ, "download_meta", _mock_download_meta)
+
+
+@pytest.fixture(name="mock_download_get")
+def fixture_mock_download_get(monkeypatch):
+    """Mock pygbif.occ.download_get to write a file to output_path."""
+
+    def _mock_download_get(*args, **kwargs):
+        with open(args[1], "wb") as f:
+            f.write(b"test")
+
+    monkeypatch.setattr(occ, "download_get", _mock_download_get)
+
+
+def test_init_gbif_download(monkeypatch, gbif_query, gbif_download_info):
+    """Test init_gbif_download"""
+
+    def mock_download(*args, **kwargs):
+        return gbif_download_info
+
+    monkeypatch.setattr(occ, "download", mock_download)
+
+    download_key = init_gbif_download(gbif_query)
+
+    assert download_key == "0000000-000000000000000"
+
+
+def test_check_download_status(
+    monkeypatch,
+    gbif_download_info,
+    gbif_download_meta_running,
+    gbif_download_meta_success,
+    gbif_download_meta_failed,
+):
+    """Test check_download_status"""
+
+    def mock_download_meta_running(*args, **kwargs):
+        return gbif_download_meta_running
+
+    def mock_download_meta_succeeded(*args, **kwargs):
+        return gbif_download_meta_success
+
+    def mock_download_meta_failed(*args, **kwargs):
+        return gbif_download_meta_failed
+
+    monkeypatch.setattr(occ, "download_meta", mock_download_meta_running)
+    assert check_download_status(gbif_download_info[0]) == "RUNNING"
+
+    monkeypatch.setattr(occ, "download_meta", mock_download_meta_succeeded)
+    assert check_download_status(gbif_download_info[0]) == "SUCCEEDED"
+
+    monkeypatch.setattr(occ, "download_meta", mock_download_meta_failed)
+    assert check_download_status(gbif_download_info[0]) == "FAILED"
+
+
+def mock_download_get(
+    key: str, output_path: str, *args, **kwargs  # pylint: disable=unused-argument
+) -> None:
+    """Mock pygbif.occurences.download_get to write a throwaway file."""
+
+    with open(Path(output_path) / f"{key}.zip", "wb") as f:
+        f.write(b"test")
+
+
+def test_download_request_to_disk(
+    tmp_path, mocker, gbif_download_info, gbif_download_meta_success
+):
+    """Test download_request_to_disk"""
+    mocker.patch(f"{TESTED_MODULE}.occ.download_get", side_effect=mock_download_get)
+    mocker.patch(
+        f"{TESTED_MODULE}.occ.download_meta", return_value=gbif_download_meta_success
+    )
+    download_request_to_disk(gbif_download_info[0], tmp_path)
+    correct_file = tmp_path / f"{gbif_download_info[0]}.zip"
+
+    assert correct_file.exists()
+    assert correct_file.stat().st_size > 0
+    assert correct_file.with_suffix(".json").exists()
+    assert correct_file.with_suffix(".json").stat().st_size > 0
+
+    ## Pytest doesn't seem to allow you to rename files in the tmp_path directory
+    ## This causes the below test to fail.
+    download_request_to_disk(gbif_download_info[0], tmp_path, "unique_name")
+    correct_file = tmp_path / "unique_name.zip"
+
+    assert correct_file.exists()
+    assert correct_file.stat().st_size > 0
+    assert correct_file.with_suffix(".json").exists()
+    assert correct_file.with_suffix(".json").stat().st_size > 0
+
+
+def test_check_download_job_and_download_file(tmp_path, mocker, gbif_download_info):
+    """Test check_download_job_and_download_file"""
+    output_path = tmp_path / "tmp.zip"
+    mocker.patch(f"{TESTED_MODULE}.check_download_status", return_value="FAILED")
+    with pytest.raises(GbifDownloadFailure) as excinfo:
+        check_download_job_and_download_file(gbif_download_info[0], output_path)
+
+    assert "failed." in str(excinfo.value)
