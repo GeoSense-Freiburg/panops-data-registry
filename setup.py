7f77076d6bdd15b4a9ba2222669e166a1c036857
Daniel Lusk <daniel.lusk@posteo.net>
2024-03-25 11:35:31 +0000

    rename package to panops-data-registry

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
-"""Setup file for the eo-data package."""
+"""Setup file for the panops-data-registry package."""
+
 from setuptools import find_packages, setup
 
 setup(
-    name="eo-data",
+    name="panops-data-registry",
     packages=find_packages(),
     version="0.1.0",
-    description="A registry of Earth observation datasets used in the Geosense lab",
+    description="A registry of Earth observation datasets used in the Geosense lab for the PANOPS project",
     author="Daniel Lusk",
     license="MIT",
 )
